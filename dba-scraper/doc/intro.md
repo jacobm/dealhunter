0eacf89bedbfce257d6cf9caa87ad47541160a22
Jacob Mortensen <jacob.mortensen.it@gmail.com>
2014-04-11 12:37:31 -0700

    initial commit

---
+++ dba-scraper/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to dba-scraper
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ dba-scraper/test/dba_scraper/core_test.clj
@@ -0,0 +1,7 @@
+(ns dba-scraper.core-test
+  (:require [clojure.test :refer :all]
+            [dba-scraper.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
