72b5b578c4ae0e76a27214292d5c434dbe07faf7
Jacob Mortensen <jacob.mortensen.it@gmail.com>
2014-04-11 23:41:50 -0700

    Run smoke test against dingo search.

--- dba-scraper/test/dba_scraper/core_test.clj
+++ dba-scraper/test/dba_scraper/core_test.clj
@@ -1,7 +1,35 @@
 (ns dba-scraper.core-test
-  (:require [clojure.test :refer :all]
-            [dba-scraper.core :refer :all]))
+  (:use [midje.sweet]
+        [dba-scraper.core])
+  (:require [clojure.string :as str]
+            [clj-time.core :as time]
+            [clj-time.format :as format]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn dingo-search []
+  (read-string (slurp "./test/dba_scraper/dingo-search.txt")))
+
+(fact "Expected number of items is found"
+  (count (get-items (dingo-search))) => 13)
+
+(fact "Extraction works as expected for first item"
+  (let [node (extract (first (get-listings (dingo-search))))]
+    (:date node) => "I dag"
+    (:headline node) => ""
+    (:link node) => "http://www.dba.dk/ballerinasko-str-findes-i/id-93107954/"
+    (:location node) => "1000 København K"
+    (:price node) => "Kr. 507"
+    (:thumbnail node) => "http://dbastatic.dk/pictures/pictures/admanager/6b/11/f276-bcf9-4b69-a30d-0a3eba572d9a.jpg?preset=srpgallery"
+    (:thumbnail-alt node) => nil
+    (:text node) => ""
+    (:text-alt node) => "Ballerinasko, str. findes i flere str., Aster Dingo Kid, piger, Ballerinaer til børn Aster  DINGO KID  Marineblå Fås i pigestørrelse. 28,29,30,31,32. . Barn &gt; Pige &gt; Sko &gt; Ballerinaer.\n\nGratis levering ved køb over 300 kr. + 30 dages gratis returnering på Spartoo.dk\n\nSpartoo er specialist i salg af mærkevaresko på internettet..."))
+
+(fact "First item is read in as expected"
+  (let [item (first (get-items (dingo-search)))]
+    (:date item) => (str (time/today))
+    (:headline item) => ""
+    (:link item) => "http://www.dba.dk/ballerinasko-str-findes-i/id-93107954/"
+    (:location item) => "1000 København K"
+    (:price item) => 507
+    (:thumbnail item) => "http://dbastatic.dk/pictures/pictures/admanager/6b/11/f276-bcf9-4b69-a30d-0a3eba572d9a.jpg?preset=srpgallery"
+    (:text item) => "Ballerinasko, str. findes i flere str., Aster Dingo Kid, piger, Ballerinaer til børn Aster  DINGO KID  Marineblå Fås i pigestørrelse. 28,29,30,31,32. . Barn &gt; Pige &gt; Sko &gt; Ballerinaer.\n\nGratis levering ved køb over 300 kr. + 30 dages gratis returnering på Spartoo.dk\n\nSpartoo er specialist i salg af mærkevaresko på internettet..."))
+
