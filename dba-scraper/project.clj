8ce1a8a66a8aa1d515dc7578e65f2142c8b8dbbd
Jacob Mortensen <jacob.mortensen.it@gmail.com>
2014-04-13 03:09:38 -0700

    Add ring and compojure.

--- dba-scraper/project.clj
+++ dba-scraper/project.clj
@@ -6,6 +6,11 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-time "0.6.0"]
                  [http-kit "2.1.16"]
-                 [enlive "1.1.5"]]
-  :plugins [[lein-midje "3.1.3"]]
+                 [enlive "1.1.5"]
+                 [compojure "1.1.6"]
+                 [ring "1.2.2"]
+                 [ring-cors "0.1.0"]]
+  :plugins [[lein-midje "3.1.3"]
+            [lein-ring "0.8.10"]]
+  :ring {:handler dba-scraper.server/app}
   :profiles {:dev {:dependencies [[midje "1.6.3"]]}})
