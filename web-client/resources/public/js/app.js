29407e9be0b774df6ebe5c5f2631a650e47dc10c
Jacob Mortensen <jacob.mortensen.it@gmail.com>
2014-05-23 22:13:06 +0200

    Feed reader, navigation etc.

--- web-client/resources/public/js/app.js
+++ web-client/resources/public/js/app.js
@@ -111,7 +111,7 @@ var MonitorTable = React.createClass({
 
 var App = React.createClass({
     getInitialState: function() {
-	$.get("http://localhost:3000/user/" + this.props.data.userId, function(data) {
+	$.get("/" + this.props.data.userId + "/monitors", function(data) {
 	    console.dir(data);
  	    this.setState({monitors: data.monitors});
 	}.bind(this));
@@ -147,27 +147,175 @@ var root = React.renderComponent(<App data={data} />,
 //		      document.getElementById('app'));
 
 
+var FeedReader = function(feed) {
+
+    var item = {};
+
+    var readBackwardsHelper = function(link, steps, itemCallback, acc) {
+	if(steps <= 0 || link === undefined) {
+	    console.dir(acc);
+	    _.each(acc.reverse(), function(accFunc) { accFunc(); });
+	    return;
+	}
+	$.get(link.href, function(data) {
+	    if(data._links.prev != undefined) {
+		acc.push(function() { itemCallback(data, link.href); });
+		readBackwardsHelper(data._links.prev, --steps, itemCallback, acc);
+		};
+	    });
+    };
+
+    // delivers items to itemCallback in order oldest -> newest
+    var readBackwards = function(link, steps, itemCallback) {
+	readBackwardsHelper(link, steps, itemCallback, []);
+    };
+
+    var readForwards = function(link, steps, itemCallback) {
+	if(steps <= 0 || link === undefined) {
+	    return;
+	}
+	$.get(link.href, function(data) {
+	    itemCallback(data, link);
+	    if(data._links.next != undefined) {
+		readForwards(data._links.next, --steps, itemCallback);
+	    }
+	});
+    };
+
+    item.readBackwards = readBackwards;
+    item.readForwards = readForwards;
+
+    item.readNewest = function(count, itemCallback) {
+	$.get("http://localhost:3000/feed/stokke", function(data) {
+	    if(data._links.newest != undefined) {
+		readBackwards(data._links.newest, count, itemCallback);
+	    }
+	});
+    }
+
+    return item;
+};
+
+var reader = FeedReader("http://localhost:3000/feed/stokke");
+
 var SearchTest = React.createClass({
+
+    traverseForward: function(link, steps, itemCallback){
+	if(steps <= 0 || link === undefined) {
+	    return;
+	}
+	console.log("getting " + link.href);
+	$.get(link.href, function(data) {
+	    if(data._links.next != undefined) {
+		itemCallback(data);
+		this.traverseForward(data._links.next, --steps, itemCallback);
+	    }
+	}.bind(this));
+    },
+
+    checkStream: function(){
+	$.get("http://localhost:3000/feed/stokke", function(data) {
+	    console.dir(data);
+	    if(data._links.next != undefined) {
+		this.traverseForward(data._links.next, 20, function(item) {
+		    //console.dir(item);
+		});
+	    }
+	}.bind(this));
+    },
     onClick: function() {
 	console.log("Clicked");
 
-	$.get("http://localhost:3000/search-one?name=stokke", function(data) {
-	    console.dir(data);
+	reader.readNewest(4, function(data, link) {
+	    console.log(data._embedded.text);
+	    console.log(link);
+	});
+	return;
+	$.get("http://localhost:3000/feed/stokke", function(data) {
+	    console.dir(data._embedded.text);
 	});
 
 	
     },
 
+    createStream: function(){
+	console.dir("her");
+	$.post("http://localhost:3000/feed/stokke", function(data) {
+	    console.dir("Received ");
+	    console.dir(data);
+	});
+    },
+
     render: function() {
 	return (
-	    <button onClick={this.onClick}>Query</button>
+	    <div>
+		<button onClick={this.onClick}>Query</button>
+        	<button onClick={this.createStream}>Create Stream</button>
+        	<button onClick={this.checkStream}>ReadStream</button>
+            </div>
 	);
     }
 });
 React.renderComponent(<SearchTest />, document.getElementById('example'));
 
 
+var TabLocation = React.createClass({
+
+    componentDidMount: function() {
+	crossroads.addRoute(this.props.section);
+    },
+    buttonClick: function(event) {
+	event.preventDefault();
+	crossroads.parse('/' + this.props.section);
+	hasher.setHash(this.props.section);
+	this.props.f();
+    },
+    render: function() {
+	return(
+	    <button onClick={this.buttonClick}>{this.props.text}</button>
+	);
+    }
+});
+
+var NavigationBar = React.createClass({
+    render: function() {
+	return (<div></div>);
+    }
+});
+
+var test = function(){ console.log("dingo"); };
+
+React.renderComponent(<TabLocation section="section1" text="section1" />, 
+		      document.getElementById('button1'));
+React.renderComponent(<TabLocation section="section2" text="section2" />, 
+		      document.getElementById('button2'));
+React.renderComponent(<TabLocation section="section3" text="section3" f={test} />, 
+		      document.getElementById('button3'));
+React.renderComponent(<TabLocation section="section4" text="section4" f={test} />, 
+		      document.getElementById('button3'));
 
 
+function init() {
+    var productRoutes = crossroads.addRoute("section1", function (id) {
+    });
 
+    var default_routes = crossroads.addRoute("/section2", function (source) {
+    });
 
+    crossroads.parse(document.location.pathname); /*This is where the parser function is called to match against the routes defined*/
+
+    crossroads.routed.add(console.log, console);
+}
+
+//setup hasher
+function parseHash(newHash, oldHash){
+  crossroads.parse(newHash);
+}
+hasher.initialized.add(parseHash); //parse initial hash
+hasher.changed.add(parseHash); //parse hash changes
+hasher.init(); //start listening for history change
+ 
+//update URL fragment generating new history record
+hasher.setHash('lorem/ipsum');
+
+;$(init);
