11f96729771107f9d15a7a4240a901284f0a06ea
Jacob Mortensen <jacob.mortensen.it@gmail.com>
2014-05-28 06:21:52 +0200

    Hookup change function.

--- web-client/resources/public/js/components/SearchResultTable.react.js
+++ web-client/resources/public/js/components/SearchResultTable.react.js
@@ -4,13 +4,34 @@ var React = require('react');
 var AppActions = require('../actions/AppActions');
 var SearchStore = require('../stores/SearchStore');
 
+function getSearchResult() {
+    return {result: SearchStore.getSearchResult()};
+}
+
 var SearchResultTable = React.createClass({
+ 
+    getInitialState: function() {
+	return getSearchResult();
+    },
+   
+    componentDidMount: function() {
+	SearchStore.addChangeListener(this._onChange);
+    },
+
+    componentWillUnmount: function() {
+	SearchStore.removeChangeListener(this_onChange);
+    },
+    
     render: function() {
 	return (
 		<div>
-		SearchTableResult
-		</div>
+		SearchTableResult {this.state.result.length}
+	    </div>
 	);
+    },
+    
+    _onChange: function() {
+	this.setState(getSearchResult());
     }
 });
 
