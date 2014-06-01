9a76714014c052a06f2819201f998d4fe13d9249
Jacob Mortensen <jacob.mortensen.it@gmail.com>
2014-06-01 14:49:40 +0200

    Set clientid.

--- web-client/resources/public/js/constants/Configuration.js
+++ web-client/resources/public/js/constants/Configuration.js
@@ -1,5 +1,5 @@
 
 module.exports = {
     SEARCH_ENDPOINT : "http://localhost:3000",
-    GOOGLE_CLIENT_ID : "Your google client id here"
+    GOOGLE_CLIENT_ID : "108491861456-g9oajn3u17m0dc6e0fu1o8phoeju2v1d.apps.googleusercontent.com"
 };
