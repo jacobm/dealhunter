c48dc121c9e696b72ef1234a497d6de0f149ab53
Jacob Mortensen <jacob.mortensen.it@gmail.com>
2014-06-02 03:13:24 +0200

    Add http-kit and clout

--- web-client/project.clj
+++ web-client/project.clj
@@ -4,6 +4,8 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.6"]
                  [ring "1.2.2"]
+                 [clout "1.2.0"]
+                 [http-kit "2.1.16"]
                  [org.clojure/data.json "0.2.4"]
                  [com.novemberain/monger "1.7.0"]]
   :plugins [[lein-ring "0.8.10"]]
