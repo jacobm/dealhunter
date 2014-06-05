b12b4300168a189e7fed3dc5a35d7822d4e2439a
Jacob Mortensen <jacob.mortensen.it@gmail.com>
2014-06-05 20:56:42 +0200

    Update SearchStore to contain users previous search items.

--- web-client/resources/public/js/stores/SearchStore.js
+++ web-client/resources/public/js/stores/SearchStore.js
@@ -3,23 +3,60 @@ var EventEmitter = require('events').EventEmitter;
 var Configuration = require('../constants/Configuration');
 var AppConstants = require('../constants/AppConstants');
 var merge = require('react/lib/merge');
+var FeedReader = require('./FeedReader');
+
+var UserStore = require('./UserStore');
 
 var CHANGE_EVENT = 'change';
 
-var _searchResult = [];
+var _search = {
+    user: {isLoggedIn: false},
+    result: [],
+    previous: []
+};
 
 function search(text) {
     $.get(Configuration.SEARCH_ENDPOINT + "/search-one?name=" + text, function(data) {
 	console.dir(data);
-	_searchResult = data;
+	_search.result = data;
 	SearchStore.emitChange();
+    });
+};
+
+function fetchNewSearchItems(previousSearch) {
+    $.get(previousSearch.url, function(feedItem) {
+        FeedReader.readForwards(feedItem._links.next, 200, function(item){
+            previousSearch.newItems.push(item);
+            SearchStore.emitChange();
+        });
+    });
+};
+
+function setPreviousSearches(userId) {
+    _search.previous = 
+        [
+        {searchTerm: "dingo", "url": "http://localhost:3000/feed/dingo", newItems: []},
+        {searchTerm: "fisk", "url": "http://localhost:3000/feed/fisk", newItems: []},
+        {searchTerm: "stokke", 
+         url: "http://localhost:3000/feed/stokke/53908daf07de7150ffffa4b5",
+         newItems: []},
+    ];
+
+    SearchStore.emitChange();
+
+    _search.previous.forEach(function(x) {
+        fetchNewSearchItems(x);
     });
 };
 
 var SearchStore = merge(EventEmitter.prototype, {
     
     getSearchResult: function() {
-	return _searchResult;
+	return _search.result;
+    },
+
+    getPreviousSearches: function() {
+        return _search.previous;
     },
     
     emitChange: function() {
