05aa4b27f3ab525a0f6995f10c1d9687a8bbe4cd
Jacob Mortensen <jacob.mortensen@hotmail.com>
2014-08-30 00:04:42 +0200

    Add Builder for building docker images.

---
+++ Builder/AssemblyInfo.fs
@@ -0,0 +1,22 @@
+module Builder.AssemblyInfo
+open System.Reflection
+open System.Runtime.CompilerServices
+
+
+[<assembly: AssemblyTitle("Builder")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("jacobm")>]
+[<assembly: AssemblyTrademark("")>]
+
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
+
---
+++ Builder/Build.fsx
@@ -0,0 +1,72 @@
+﻿
+#r @"../packages/FAKE.3.3.0/tools/FakeLib.dll"
+
+open System
+open System.IO
+open System.Diagnostics
+open Fake
+
+let buildDir = "./build/"
+let scraperWebName = "ScraperWeb"
+let scraperRobotName = "DbaScraper"
+
+
+
+let buildDockerImage path tagname = 
+    let args = "build -t " + tagname + " " + path
+    trace ("docker " + args)
+    
+    let p = new Process()
+    p.StartInfo.FileName <- "docker"
+    p.StartInfo.Arguments <- "build -t " + tagname + " " + path
+    p.StartInfo.UseShellExecute <- false
+    p.StartInfo.RedirectStandardOutput <- true
+    p.Start() |> ignore
+
+    let output = p.StandardOutput.ReadToEnd()
+    p.WaitForExit()
+    trace output
+
+let buildDocker name tagName =
+    let dir = buildDir + name
+    let dockerfileName = name + "_Dockerfile"
+    trace ("copying " + dockerfileName + " to " + dir)
+    File.Copy("Builder/" + dockerfileName, dir + "/Dockerfile", true)
+    buildDockerImage dir tagName
+
+// Targets
+Target "Clean" (fun _ ->
+    CleanDir buildDir
+)
+
+Target "DbaScraper" (fun _ ->
+    !! "./DbaScraper/**/*.fsproj"
+      |> MSBuildRelease (buildDir + scraperRobotName + "/bin") "Build"
+      |> Log "AppBuild-Output: "
+)
+
+Target "ScraperWeb" (fun _ ->
+    !! "./ScraperWeb/**/*.fsproj"
+      |> MSBuildRelease (buildDir + scraperWebName + "/bin") "Build"
+      |> Log "AppBuild-Output: "
+)
+
+Target "BuildDbaScraperImage" (fun _ ->
+    buildDocker "DbaScraper" "jacobm/dba-scraper"
+)
+
+Target "BuildScraperWebImage" (fun _ ->
+    buildDocker "ScraperWeb" "jacobm/scraperweb"
+)
+
+Target "Default" (fun _ -> 
+    trace "Done")
+
+"Clean" 
+    ==> "DbaScraper"
+    ==> "ScraperWeb"
+    ==> "BuildDbaScraperImage"
+    ==> "BuildScraperWebImage"
+    ==> "Default"
+
+RunTargetOrDefault "Default"
