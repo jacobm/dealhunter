5113c074fcd98d8017c067e1b4c31449f7dfaa9b
Jacob Mortensen <jacob.mortensen@hotmail.com>
2014-08-31 09:35:30 +0200

    Build search page, move searchTextInput into this file.

---
+++ DealClient/Content/dartclient/web/components/search.react.dart
@@ -0,0 +1,102 @@
+library Search;
+
+import "package:react/react.dart" as react;
+import "../actions/app_actions.dart" as Actions;
+import "../stores/feed_store.dart";
+
+var ENTER_KEY_CODE = 13;
+
+class _SearchTextInput extends react.Component {
+  getInitialState() {
+    var init = this.props['value'];
+    if (init != null)
+    {
+      return {"value": init};
+    }
+
+    return {"value" : ""};
+  }
+
+  _onChange(event) {
+    this.setState({"value": event.target.value});
+  }
+
+  _onKeyDown(event) {
+    if (event.keyCode == ENTER_KEY_CODE) {
+      _onSubmit();
+    }
+  }
+
+  _onSubmit() {
+    var value = this.state["value"];
+    if (value != ""){
+      this.props["onSubmit"](this.state["value"]);
+      this.setState({"value": ""});
+    }
+  }
+
+  render() {
+    return react.input(
+        {"onChange": (e) => _onChange(e),
+         "onSubmit": (e) => _onSubmit(),
+         "onKeyDown": (e) => _onKeyDown(e),
+         "value" : _value,
+         "placeholder": "Search"},
+         react.div({}, [_value,
+                        react.button({
+                         "onClick": (e) => _onSubmit()}, "Search")])
+    );
+  }
+
+  String get _value => this.state["value"];
+}
+var searchTextInput = react.registerComponent(() => new _SearchTextInput());
+
+class _SearchItem extends react.Component {
+
+  SearchItem get item => this.props["item"];
+
+  render() {
+    return react.li({}, item.text);
+  }
+}
+var searchItem = react.registerComponent(() => new _SearchItem());
+
+class _Search extends react.Component {
+  FeedStore feedStore = new FeedStore();
+
+  getInitialState(){
+    return {"searchResult": feedStore.items};
+  }
+
+  componentDidMount(domNode) {
+    feedStore.Attach(_onChange);
+  }
+
+  searchTable(List<SearchItem> items) {
+    var res = items.map((x) => searchItem({"item": x}));
+    return react.ul({}, res);
+  }
+
+  render() {
+    return react.div({}, [searchTextInput({"onSubmit": _onSubmit}),
+                          searchTable(_searchResults)]);
+  }
+
+  List<SearchItem> get _searchResults => this.state["searchResult"];
+                   set _searchResults (List<SearchItem> value) =>
+                       this.setState({"searchResult": value});
+
+  _onSubmit(String text) {
+    Actions.search(text);
+  }
+
+  _onChange(String change) {
+    switch(change) {
+      case FeedStore.SearchResultReady:
+        _searchResults = feedStore.items;
+        break;
+    }
+  }
+}
+var search = react.registerComponent(() => new _Search());
--- DealClient/Content/dartclient/web/stores/feed_store.dart
+++ DealClient/Content/dartclient/web/stores/feed_store.dart
@@ -1,16 +1,83 @@
 library FeedStore;
 
 import 'dart:async';
+import 'dart:html';
+import 'dart:convert';
+import "package:json_object/json_object.dart";
+import '../constants/app_constants.dart' as AppConstants;
+import '../dispatcher/app_dispatcher.dart';
 
 class FeedStore {
+  static const SearchResultReady = "SeachResultReady";
+  List<SearchItem> _searchItems = new List<SearchItem>();
 
-    var s = new StreamController<String>();
+  static final FeedStore _singleton = new FeedStore._internal();
+    var events = new StreamController<String>();
 
-    void Attach(listener){
-      s.stream.listen(listener);
+  factory FeedStore() {
+    return _singleton;
+  }
+
+  FeedStore._internal(){
+    new AppDispatcher().attach(_onAction);
+  }
+
+  List<SearchItem> get items => _searchItems;
+
+  void Attach(listener){
+    events.stream.listen(listener);
+  }
+
+  _search(String term){
+    HttpRequest.getString("http://localhost:8888/search/" + term)
+               .then((response) {
+      var items = new JsonObject.fromJsonString(response);
+      _searchItems = items.map((x) => new SearchItem.fromMap(x)).toList();
+      events.add(SearchResultReady);
+    }).catchError((error){
+      print(error);
+    });
+  }
+
+  void _onAction(action){
+    print(action);
+    switch(action["actionType"]){
+      case AppConstants.AppSearch:
+        _search(action["payload"]["searchTerm"]);
+        break;
     }
+  }
+}
 
-    void Poke(String value){
-      s.add(value);
-    }
+class Location {
+  int postcode;
+  String city;
+
+  Location(this.postcode, this.city);
+}
+
+class SearchItem {
+  String text;
+  String thumbnail;
+  int price;
+  String dbaLink;
+  String dbaId;
+  DateTime postedAt;
+  Location location;
+
+  SearchItem(){}
+
+  factory SearchItem.fromMap(Map map) {
+    var item = new SearchItem();
+    item.text = map["text"];
+    item.thumbnail = map["thumbnail"];
+    item.price = map["price"];
+    item.dbaLink = map["dbaLink"];
+    item.dbaId = map["dbaId"];
+    String dingo = map["postedAt"];
+    item.postedAt = DateTime.parse(dingo);
+    item.location = new Location(map["postcode"], map["city"]);
+
+    return item;
+  }
 }
