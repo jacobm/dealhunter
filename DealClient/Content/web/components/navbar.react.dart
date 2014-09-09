11e0d4c93de26988af71566627b380dfcc9acafd
Jacob Mortensen <jacob.mortensen@hotmail.com>
2014-09-09 19:22:28 +0200

    Move dependency on userstore to navbar.

--- DealClient/Content/web/components/navbar.react.dart
+++ DealClient/Content/web/components/navbar.react.dart
@@ -1,33 +1,17 @@
 library Navbar;
 
-import "package:google_oauth2_client/google_oauth2_browser.dart";
-import "package:google_plus_v1_api/plus_v1_api_browser.dart" as plusclient;
 import "package:react/react.dart" as react;
 import "../actions/app_actions.dart" as Actions;
-import "../constants/app_constants.dart" as AppConstants;
 import "../stores/user_store.dart";
 
 class _GoogleLoginButton extends react.Component {
-  GoogleOAuth2 auth;
-
-  oauthReady(Token token) {
-    Actions.login(auth);
-  }
-
-  componentDidMount(e) {
-    this.auth = new GoogleOAuth2(
-        AppConstants.googleClientId,
-        ["openid", "email", plusclient.Plus.PLUS_ME_SCOPE],
-        tokenLoaded: oauthReady);
-    auth.login(immediate: true, onlyLoadToken : true);
-  }
 
   _onClick(event) {
-    auth.login(immediate: false, onlyLoadToken : true);
+    Actions.login();
   }
 
   _onLogoutClick(event) {
-    auth.logout();
+    Actions.logout();
   }
 
   render (){
@@ -39,16 +23,8 @@ class _GoogleLoginButton extends react.Component {
 var googleLoginButton = react.registerComponent(() => new _GoogleLoginButton());
 
 class _CurrentUser extends react.Component {
-  UserStore userStore;
 
-  componentWillMount() {
-    userStore = new UserStore();
-    userStore.Attach(_onUserEvent);
-  }
-
-  getInitialState(){
-    return {"User": null};
-  }
+  CurrentUser get User => this.props["user"];
 
   render() {
     if (User == null) {
@@ -65,8 +41,32 @@ class _CurrentUser extends react.Component {
   _onLogoutClick(event) {
     Actions.logout();
   }
+}
+var currentUser = react.registerComponent(() => new _CurrentUser());
+
+class _NavBar extends react.Component {
+  UserStore userStore;
 
   CurrentUser get User => this.state["User"];
+
+  componentWillMount() {
+    userStore = new UserStore();
+    userStore.Attach(_onUserEvent);
+  }
+
+  getInitialState(){
+    return {"User": null};
+  }
+
+  render() {
+    if (User == null){
+      return react.div({}, ["Navbar",
+                            googleLoginButton({})]);
+    } else {
+      return react.div({}, ["Navbar",
+                            currentUser({"user": User})]);
+    }
+  }
 
   _onUserEvent(event) {
     switch(event) {
@@ -74,20 +74,11 @@ class _CurrentUser extends react.Component {
         this.setState({"User": userStore.User});
         break;
       case UserStore.UserLoggedOutEvent:
-             this.setState({"User": userStore.User});
-             break;
+         this.setState({"User": userStore.User});
+         break;
       default:
         break;
     }
-  }
-}
-var currentUser = react.registerComponent(() => new _CurrentUser());
-
-class _NavBar extends react.Component {
-  render() {
-    return react.div({}, ["Navbar",
-                          googleLoginButton({}),
-                          currentUser({})]);
   }
 }
 var navBar = react.registerComponent(() => new _NavBar());
