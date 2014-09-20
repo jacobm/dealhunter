31fb8f4cec1c9d937db3368891f6b865370ec80c
Jacob Mortensen <jacob.mortensen@hotmail.com>
2014-09-20 19:20:33 +0200

    Add module for persisting and retrieving user data.

--- DealClient/Program.fs
+++ DealClient/Program.fs
@@ -71,6 +71,55 @@ module DealClientSettings =
         | _ -> raise (Exception("Unknown environment: " + environment))
 
 
+module Persistence =
+    open Npgsql
+    open Newtonsoft.Json
+    open DealClientTypes
+
+    let getConnection (connectionString : string) =
+        let connection =
+            new NpgsqlConnection(connectionString)
+        connection.Open()
+        connection
+
+    let dropTables connectionString =
+        use connection = getConnection connectionString
+        let command = new NpgsqlCommand(@"drop table userdata;", connection)
+        command.ExecuteNonQuery
+
+    let createTables connectionString =
+        use connection = getConnection connectionString
+        let command = new NpgsqlCommand(@"
+            create table userdata (id serial primary key, googleId numeric(25) not null, data json not null);", connection)
+        command.ExecuteNonQuery()
+
+    let getUserData connectionString (googleId : GoogleId) =
+        let (Id id) = googleId
+        use connection = getConnection connectionString
+        let command = new NpgsqlCommand(@"select data from userdata where googleId = @id", connection)
+        command.Parameters.AddWithValue("@id", id) |> ignore
+        use reader = command.ExecuteReader()
+        match reader.Read() with
+        | true -> Some(JsonConvert.DeserializeObject<UserData>(reader.GetString(0)))
+        | _ -> None
+
+    let setUserData connectionString (googleId : GoogleId) userData =
+        let (Id id) = googleId
+        let data = JsonConvert.SerializeObject(userData)
+        use connection = getConnection connectionString
+        let command = new NpgsqlCommand(@"
+             WITH upsert AS (
+                 UPDATE userdata SET data = @data
+                 WHERE googleId = @id RETURNING *)
+             INSERT INTO userdata (googleId, data) SELECT @id, @data
+             WHERE NOT EXISTS (SELECT * FROM upsert)
+            ", connection)
+        command.Parameters.AddWithValue("@id", id) |> ignore
+        command.Parameters.AddWithValue("@json", data) |> ignore
+        let result = command.ExecuteNonQuery()
+        ()
+
+
 module Site  =
     open System
     open System.Text
