ac1f32a8e6d4b027cdc5e60813b2cfae8aba09cd
Jacob Mortensen <jacob.mortensen@hotmail.com>
2014-09-21 15:43:55 +0200

    Add UserStateUpdated event.

--- DealClient/Content/web/actions/app_events.dart
+++ DealClient/Content/web/actions/app_events.dart
@@ -3,3 +3,4 @@ library Events;
 const SearchResultReady = "SeachResultReady";
 const UserLoggedInEvent = "UserLoggedInEvent";
 const UserLoggedOutEvent = "UserLoggedOutEvent";
+const UserStateUpdated = "UserStateUpdated";
