e050cb5c3e7bf3e9ec79835ba5d6ea74a1184feb
Jacob Mortensen <jacob.mortensen@hotmail.com>
2014-09-29 20:05:45 +0200

    Add test for building up state.

--- Test/DealClientTests.fs
+++ Test/DealClientTests.fs
@@ -38,6 +38,17 @@ module DealClientDomainTests =
         Assert.True(state.term = "dingo")
         Assert.True(state.position = Some "1")
 
+    [<Fact>]
+    let ``buildState return watch on dingo and hest`` () =
+        let events = [Watch "dingo"; SetTermPosition ("dingo", "1"); Watch "hest"]
+        let result = buildState events
+        Assert.True(Seq.length result = 2)
+        let dingo = Seq.find (fun x -> x.term = "dingo") result
+        Assert.True(dingo.isWatched)
+        Assert.True(dingo.position = Some "1")
+        let hest = Seq.find (fun x -> x.term = "hest") result
+        Assert.True(hest.isWatched)
+        Assert.True(hest.position = None)
 
     [<Property>]
     let ``Testings`` (x:UserEvent list) =
